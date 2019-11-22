b86406b5154bd4e9f076075a0d88e9c841ff4da6
Marwane <Marwane@10.251.172.205>
2019-11-22 14:05:10 +0100

    14.03

--- src/main/resources/public/resources/Js.js
+++ src/main/resources/public/resources/Js.js
@@ -1,15 +1,20 @@
 
+
 function inscription(event){
 event.preventDefault();
-var formData = { "lastName":""+$("#last_name").val()+"", "firstName":""+$("#first_name").val()+"","username":""+$("#username").val()+"", "password":""+$("#password").val()+"", "mail": ""+$("#email").val()+""}
+var formData = { "lastName" : $("#last_name").val() , "firstName": $("#first_name").val() ,"username":
+ $("#username").val() , "pass": $("#password").val() , "mail":  $("#email").val() }
 console.log(formData);
 $.ajax({
   type: "POST",
-  url: "127.0.0.1:8080/user/create",
-  data: formData,
-  success: function(){},
+  url: "http://127.0.0.1:8080/user/create",
+  data: JSON.stringify(formData),
+  success: function(){alert("ajouter a la base")},
   dataType: "json",
   contentType : "application/json"
 });
+
+
+
 	}				
 					
